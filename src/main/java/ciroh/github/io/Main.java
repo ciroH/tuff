1cc28612b25f2195a93aca102a963d54b7db96d6
matesuli <63865991+ciroH@users.noreply.github.com>
2024-01-24 11:33:50 -0300

    Added Main and UITest Classes

---
+++ src/main/java/ciroh/github/io/Main.java
@@ -0,0 +1,10 @@
+package ciroh.github.io;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		
+	}
+
+}
---
+++ src/test/java/ciroh/github/io/UITest.java
@@ -0,0 +1,13 @@
+package ciroh.github.io;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+class UITest {
+
+	@Test
+	void test() {
+		
+	}
+
+}
