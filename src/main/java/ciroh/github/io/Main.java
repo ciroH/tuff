f9c7ecbbfb7de57bfb72a92e694f8bd40cfeecdb
matesuli <63865991+ciroH@users.noreply.github.com>
2024-01-27 11:38:32 -0300

    Added basic setup and text area

--- src/main/java/ciroh/github/io/Main.java
+++ src/main/java/ciroh/github/io/Main.java
@@ -1,10 +1,42 @@
 package ciroh.github.io;
 
-public class Main {
+import java.awt.Dimension;
 
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.JTextArea;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
+
+import com.formdev.flatlaf.FlatDarkLaf;
+
+public class Main extends JFrame {
+	JTextArea textArea;
+	
 	public static void main(String[] args) {
-		
+		try {
+		    UIManager.setLookAndFeel(new FlatDarkLaf());
+		} catch( UnsupportedLookAndFeelException ex ) {
+		    JOptionPane.showMessageDialog(null, "Failed to initialize LaF: \n" + ex.getMessage());
+		}
+		//do this with a Runnable instead
+		Main mainWindow = new Main();
 		
 	}
 
+	public Main() {
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setVisible(true);
+		this.setLocationRelativeTo(null);
+		this.setMinimumSize(new Dimension(600, 450));
+		this.setExtendedState(JFrame.MAXIMIZED_BOTH | JFrame.MAXIMIZED_HORIZ);
+		this.setTitle("tuff -- EARLY BUILD");
+		
+		textArea = new JTextArea();
+		textArea.setLineWrap(true);
+		textArea.setWrapStyleWord(true);
+		this.add(textArea);
+		
+	}
+	
 }
